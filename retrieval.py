497d8263543872e611c0f2b096c1e17d44cbf2dc
Sarbo Sarcar <sarbosarcar@Sarbos-MacBook-Air.local>
2025-06-27 15:41:06 +0530

    FIX: Modified LLM and endpoint

--- retrieval.py
+++ retrieval.py
@@ -5,9 +5,9 @@ import dotenv
 dotenv.load_dotenv()
 API_KEY = os.environ.get("HUGGINGFACEHUB_API_KEY")
 
-def retrieve(prompt, stream=True, model="HuggingFaceH4/zephyr-7b-beta"):
+def retrieve(prompt, stream=True, model="meta-llama/Llama-3.1-8B-Instruct"):
     client = OpenAI(
-        base_url="https://api-inference.huggingface.co/v1/",
+        base_url="https://router.huggingface.co/featherless-ai/v1",
         api_key=f"{API_KEY}"
     )
     messages = [
@@ -23,3 +23,5 @@ def retrieve(prompt, stream=True, model="HuggingFaceH4/zephyr-7b-beta"):
         stream=stream
     )
     return result
+
+print(retrieve("describe photosynthesis", stream=False).choices[0].message.content)
